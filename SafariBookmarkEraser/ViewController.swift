175bb0489afcbd7ee86770203a70f23669b1dba0
vascarpenter <gikoha5666@gmail.com>
2022-03-25 11:00:22 +0900

    Initial Commit

---
+++ SafariBookmarkEraser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SafariBookmarkEraser
+//
+//  Created by gikoha on 2022/03/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
